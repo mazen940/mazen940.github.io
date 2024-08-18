4cc0c78f10be5e3e870c8ce0c874e93233f2c4f1
mazen940 <mazen940@users.noreply.github.com>
2024-08-18 12:11:30 +0300

    cyborg

---
+++ React-Projects/cypbrog/src/sections/Footer/Footer.js
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <p>Copyright 2024 <a href='/#'>Cyborg Gaming</a> Company All rights Reserved</p>
+          </div>
+        </div>
+      </div>
+      
+    </footer>  )
+}
+
+export default Footer
